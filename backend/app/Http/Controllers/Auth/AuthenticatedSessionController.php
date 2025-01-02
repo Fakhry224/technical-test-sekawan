94cc58b3fa49b8edb867bd825b1f6124ec4b855b
Fakhry224 <fakhryzahranhakim@gmail.com>
2025-01-02 14:44:23 +0700

    Delete: database.sql

--- backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -20,10 +20,9 @@ class AuthenticatedSessionController extends Controller
         }
 
         $user = Auth::user();
-        $token = $user->createToken('auth_token')->plainTextToken;
+
 
         return response()->json([
-            'access_token' => $token,
             'token_type' => 'Bearer',
             'user' => $user,
             'status' => 'Login successful',
